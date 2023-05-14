4d7831e9befb5e6b795178204736a59c46f912ee
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-05-14 20:55:59 +0330

    permission dtos added.

---
+++ server/src/dtos/permission/addPermissionDTO.ts
@@ -0,0 +1,5 @@
+export interface AddPermissionDTO{
+  name: string;
+  description?: string;
+}
+
---
+++ server/src/dtos/permission/updatePermissionDTO.ts
@@ -0,0 +1,7 @@
+import { BaseModel } from "../../models/shared/base/baseModel";
+
+export interface UpdatePermissionDTO extends BaseModel {
+  name: string;
+  description?: string;
+}
+
