9b56635d0f85d7cea09bc3deb3be0e1ef133ed96
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-05-15 21:52:06 +0330

    pagePermissionValidation added.

---
+++ server/src/validations/security/pagePermissionValidation.ts
@@ -0,0 +1,115 @@
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import AppConstant from "src/constants/appConstants";
+import Message from "src/constants/messages";
+import { CRUDResultModel } from "src/models/shared/crud/crudResultModel";
+import { MethodError } from "src/models/shared/crud/methodError";
+import { MethodResult } from "src/models/shared/crud/methodResult";
+import { CRUDResultEnum } from "src/models/shared/enums/crudResultEnum";
+
+const joi = require("joi");
+const options = {
+     abortEarly: false,
+     allowUnknown: true,
+     stripUnknown: true 
+ };
+
+const addValidation = (req: Request, res: Response, next: NextFunction) => {
+     const addValidationSchema = joi.object({
+          pageId: joi.string().trim(true).required().label('page')
+          .messages({ 
+               "string.base": req.t('pageIdIsRequired', `{{#label}} is required`),
+               "string.empty": req.t('pageIdIsRequired', `{{#label}} is required`),
+               "string.required": req.t('pageIdIsRequired', `{{#label}} is required`)}),
+          permission: joi.string().trim(true).required().label('Description')
+          .messages({ 
+               "string.base": req.t('permissionIsRequired', `{{#label}} is required`),
+               "string.empty": req.t('permissionIsRequired', `{{#label}} is required`),
+               "string.max": req.t('maxLenghtForLastNameIsN', `{{#label}} max lenght is {{#limit}}`, {n: `{{#limit}}`})})          
+     });
+     
+     const { error } = addValidationSchema.validate(req.body, options);
+      
+      if (error) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, Message.UnknownErrorHappened, error.details.map((x:any) => <MethodError>{
+               Title: x.message,
+               Description: x.message
+          }))));
+      } else {
+         next();
+      }
+}
+
+
+const getByIdValidation = (req: any, res: Response, next: NextFunction) => {
+     const getByIdSchema = joi.string().trim(true).required().label('Id')
+     .messages({ 
+          "string.base": req.t('idIsRequired', `{{#label}} is required`),
+          "string.empty": req.t('idIsRequired', `{{#label}} is required`),
+          "string.required": req.t('idIsRequired', `{{#label}} is required`)
+     });
+     
+     const { error } = getByIdSchema.validate(req.body, options);
+
+     if (error) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, Message.UnknownErrorHappened, error.details.map((x:any) => <MethodError>{
+               Title: x.message,
+               Description: x.message
+          }))));
+      } else {
+         next();
+      }
+}
+
+const updateValidation = (req: any, res: Response, next: NextFunction) => {
+     const updateValidationSchema = joi.object({
+          pageId: joi.string().alphanum().max(AppConstant.NameMaxLenght).trim(true).required().label('Name')
+          .messages({ 
+               "string.base": req.t('pageIdIsRequired', `{{#label}} is required`),
+               "string.empty": req.t('pageIdIsRequired', `{{#label}} is required`),
+               "string.required": req.t('pageIdIsRequired', `{{#label}} is required`),
+               "string.max": req.t('maxLenghtForFirstNameIsN', `{{#label}} max lenght is {{#limit}}`, {n: `{{#limit}}`})}),
+          permission: joi.string().max(AppConstant.DescriptionMaxLenght).trim(true).required().label('Description')
+          .messages({ 
+               "string.base": req.t('permissionIsRequired', `{{#label}} is required`),
+               "string.max": req.t('maxLenghtForLastNameIsN', `{{#label}} max lenght is {{#limit}}`, {n: `{{#limit}}`})})
+     });
+     
+  
+      const { error } = updateValidationSchema.validate(req.body, options);
+      
+      if (error) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, Message.UnknownErrorHappened, error.details.map((x:any) => <MethodError>{
+               Title: x.message,
+               Description: x.message
+          }))));
+      } else {
+         next();
+      }
+}
+
+const deleteValidation = (req: any, res: Response, next: NextFunction) => {
+     const deleteSchema = joi.string().trim(true).required().label('Id')
+     .messages({ 
+          "string.base": req.t('idIsRequired', `{{#label}} is required`),
+          "string.empty": req.t('idIsRequired', `{{#label}} is required`),
+          "string.required": req.t('idIsRequired', `{{#label}} is required`)});
+     
+     const { error } = deleteSchema.validate(req.body, options);
+      
+      if (error) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, Message.UnknownErrorHappened, error.details.map((x:any) => <MethodError>{
+               Title: x.message,
+               Description: x.message
+          }))));
+      } else {
+         next();
+      }
+}
+
+export { 
+     addValidation,
+     getByIdValidation,
+     updateValidation,
+     deleteValidation
+     };
