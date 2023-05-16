d9fda5e0c5a8571699620a6e7264a738f5b2d6d6
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-05-16 14:04:42 +0330

    userRoleRouter added.

---
+++ server/src/routers/security/userRoleRouter.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { UserRoleController } from '../../controllers/security/userRoleController';
+import {container} from 'tsyringe'; 
+import { addValidation, deleteValidation, getByIdValidation, updateValidation } from 'src/validations/security/rolePagePermissionValidation';
+
+const userRoleController = container.resolve(UserRoleController);
+const router = express.Router();
+
+router.post('/add', addValidation, userRoleController.add);
+router.post('/fetchAll', userRoleController.getAll);
+router.post('/getById', getByIdValidation, userRoleController.getById);
+router.post('/update', updateValidation, userRoleController.update);
+router.post('/delete', deleteValidation, userRoleController.delete);
+
+export default router;
