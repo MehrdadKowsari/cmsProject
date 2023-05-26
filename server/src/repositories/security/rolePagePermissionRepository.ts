86b81e4a6a8cba154a5ac550733683effb43d13c
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-05-26 18:34:40 +0330

    rolePagePermissionRepository to adding method to check permission updated.

--- server/src/repositories/security/rolePagePermissionRepository.ts
+++ server/src/repositories/security/rolePagePermissionRepository.ts
@@ -2,6 +2,13 @@ import { GridParameter } from "src/dtos/shared/grid/gridPrameter";
 import RolePagePermissionModel, { RolePagePermission } from "src/models/security/rolePagePermission";
 import GridUtilityHelper from "src/helpers/gridUtilityHelper";
 import AppConstant from "src/constants/appConstants";
+import PagePermissionModel, { PagePermission } from "src/models/security/pagePermission";
+import UserRoleModel, { UserRole } from "src/models/security/userRole";
+import { PageTypeEnum } from "src/enums/security/pageEnum";
+import { PermissionTypeEnum } from "src/enums/security/permissionTypeEnum";
+import PermissionModel, { Permission } from "src/models/security/permission";
+import PageModel, { Page } from "src/models/security/page";
+import { Types } from "mongoose";
 
 
     export default class RolePagePermissionRepository{
@@ -66,9 +73,43 @@ import AppConstant from "src/constants/appConstants";
          * @param {string} pagePermissionId 
          * @returns {Promise<boolean>}
          */
+         
          isDuplicate = async (id: string | null, roleId: string, pagePermissionId: string) : Promise<boolean> => 
          {
              return id ? await RolePagePermissionModel.count({ roleId, pagePermissionId, _id: {$ne: id}}) > 0 : await RolePagePermissionModel.count({ roleId, pagePermissionId}) > 0;  
+         }
+         
+         /**
+         * is permission exists permission by pageId
+         * 
+         * @param {string} pageId 
+         * @param {string} permissionId 
+         * @param {string} userId 
+         * @returns {Promise<boolean>}
+         */
+         
+         hasPermissionByPageIdAndPermissionIdAndRoleList = async (pageId: PageTypeEnum, permissionId: PermissionTypeEnum, userId: string) : Promise<boolean> => 
+         {
+            const userRoles: UserRole[] | null = (await UserRoleModel.find({userId: userId}));
+            if (!userRoles || userRoles.length === 0) {
+                return Promise.resolve(false);
+            }
+            const permission: Permission | null = (await PermissionModel.findOne({type: permissionId}));
+            if (!permission) {
+                return Promise.resolve(false);
+            }
+            const page: Page | null = (await PageModel.findOne({type: pageId}));
+            if (!page || !page.isActive) {
+                return Promise.resolve(false);
+            }
+            
+            const roleIds: Types.ObjectId[] = userRoles.map(p => new Types.ObjectId(p.roleId!.toString()));
+        
+            const pagePermission: PagePermission | null = await PagePermissionModel.findOne({pageId: page._id, permissionId: permission._id});
+            if (!pagePermission) {
+                return Promise.resolve(false);
+            }
+             return await RolePagePermissionModel.count({ pagePermissionId: pagePermission._id?.toString(), roleId: {$in: roleIds}}) > 0;  
          }
         
         /**
