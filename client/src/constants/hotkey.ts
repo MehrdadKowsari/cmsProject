5509815686dee1063d72226c3aff7b687072fb24
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-03 11:44:46 +0330

    client: hotkey constant file added.

---
+++ client/src/constants/hotkey.ts
@@ -0,0 +1,9 @@
+export default class Hotkey {
+    public static readonly New: string = 'alt+n';
+    public static readonly Save: string = 'alt+s';
+    public static readonly Add: string = 'alt+i';
+    public static readonly Update: string = 'alt+u';
+    public static readonly Delete: string = 'alt+d';
+    public static readonly ToggleActive: string = 'alt+t';
+    
+}
