f9d8c8217656f00f12ca57915b5f63a6ed1c5f22
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-04 21:32:46 +0330

    client: some hotkey updated in Hotkey constans file.

--- client/src/constants/hotkey.ts
+++ client/src/constants/hotkey.ts
@@ -1,9 +1,10 @@
 export default class Hotkey {
     public static readonly New: string = 'alt+n';
     public static readonly Save: string = 'alt+s';
+    public static readonly Reset: string = 'alt+z';
     public static readonly Add: string = 'alt+i';
     public static readonly Update: string = 'alt+u';
-    public static readonly Delete: string = 'alt+d';
+    public static readonly Delete: string = 'alt+r';
     public static readonly ToggleActive: string = 'alt+t';
     
 }
