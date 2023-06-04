82967cfc6537f3ecae08da5acb40863ce8877a02
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-04 21:30:24 +0330

    client: type added into pageDTO

--- server/src/dtos/security/page/pageDTO.ts
+++ server/src/dtos/security/page/pageDTO.ts
@@ -1,9 +1,11 @@
+import { PageTypeEnum } from "src/enums/security/pageEnum";
 import { BaseModel } from "../../../models/shared/base/baseModel";
 
 export interface PageDTO extends BaseModel {
     parentId?: string;
     parentName: string;
     name: string;
+    type: PageTypeEnum,
     priority: number;
     iconClass?: string;
     isActive: boolean;
