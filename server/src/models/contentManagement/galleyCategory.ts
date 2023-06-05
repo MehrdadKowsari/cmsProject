e6642590e91b542a47dd0e7a5767c0532c2f631f
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-05 15:56:17 +0330

    server: galleryCategory model added.

---
+++ server/src/models/contentManagement/galleyCategory.ts
@@ -0,0 +1,69 @@
+import { Schema, model, Types } from 'mongoose';
+
+export const DOCUMENT_NAME = 'GalleyCategory';
+export const COLLECTION_NAME = 'galleryCategories';
+
+export  interface GalleyCategory {
+  _id: Types.ObjectId | null;
+  parentId?: Types.ObjectId;
+  name: string;
+  priority: number;
+  iconClass?: string;
+  isActive: boolean;
+  isHidden: boolean;
+  createdAt?: Date;
+  createdBy?: Types.ObjectId;
+  updatedAt?: Date;
+  updatedBy?: Types.ObjectId;
+}
+
+const schema = new Schema<GalleyCategory>(
+  {
+    parentId: {
+      type: Schema.Types.ObjectId,
+      ref:'GalleyCategory'
+    },
+    name: {
+      type: Schema.Types.String,
+      required: true,
+      maxlength: 100
+    },
+    priority: {
+      type: Schema.Types.Number,
+      required: true
+    },
+    iconClass: {
+      type: Schema.Types.String,
+      maxlength: 100
+    },
+    isActive: {
+      type: Schema.Types.Boolean,
+      default: true,
+      required: true
+    },
+    isHidden: {
+      type: Schema.Types.Boolean,
+      default: false,
+      required: true
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref:'User'
+    },
+    createdAt: {
+      type: Schema.Types.Date,
+      required: true,
+      dafault: Date.now()
+    },
+    updatedAt: {
+      type: Schema.Types.Date
+    },
+    updatedBy: {
+      type: Schema.Types.ObjectId,
+      ref:'User'
+    },
+  }
+);
+
+const GalleyCategoryModel = model<GalleyCategory>(DOCUMENT_NAME, schema, COLLECTION_NAME);
+export default GalleyCategoryModel;
