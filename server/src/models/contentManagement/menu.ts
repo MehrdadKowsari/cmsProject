714e304d5c2553b68a84d7b6a21466e2b7420e18
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-05 14:26:57 +0330

    server: menu model added.

---
+++ server/src/models/contentManagement/menu.ts
@@ -0,0 +1,58 @@
+import { Schema, model, Types } from 'mongoose';
+
+export const DOCUMENT_NAME = 'Menu';
+export const COLLECTION_NAME = 'menus';
+
+export  interface Menu {
+  _id: Types.ObjectId | null;
+  name: string | null;
+  description: string | null;
+  sectionName: string | null;
+  priority: number | null;
+  isActive: boolean;
+  createdAt?: Date;
+  createdBy?: Types.ObjectId;
+  updatedAt?: Date;
+  updatedBy?: Types.ObjectId;
+}
+
+const schema = new Schema<Menu>(
+  {
+    name: {
+      type: Schema.Types.String,
+      maxlength: 200
+    },
+    description: {
+      type: Schema.Types.String,
+      maxlength: 2000
+    },
+    sectionName: {
+      type: Schema.Types.String,
+      maxlength: 200
+    },
+    isActive: {
+      type: Schema.Types.Boolean,
+      required: true,
+      default: true
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref:'User'
+    },
+    createdAt: {
+      type: Schema.Types.Date,
+      required: true,
+      dafault: Date.now()
+    },
+    updatedAt: {
+      type: Schema.Types.Date
+    },
+    updatedBy: {
+      type: Schema.Types.ObjectId,
+      ref:'User'
+    },
+  }
+);
+
+const PostModel = model<Menu>(DOCUMENT_NAME, schema, COLLECTION_NAME);
+export default PostModel;
