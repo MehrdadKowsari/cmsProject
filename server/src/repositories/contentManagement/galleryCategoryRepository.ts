48bca5a46f61300f71e900e948ef88f03d358df1
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-05 19:52:11 +0330

    galleryCategoryRepository added.

---
+++ server/src/repositories/contentManagement/galleryCategoryRepository.ts
@@ -0,0 +1,119 @@
+import { GridParameter } from "src/dtos/shared/grid/gridPrameter";
+import GalleryCategoryModel, { GalleryCategory } from "src/models/contentManagement/galleryCategory";
+import GridUtilityHelper from "src/helpers/gridUtilityHelper";
+import AppConstant from "src/constants/appConstants";
+
+
+    export default class GalleryCategoryRepository{
+        /**
+         * add galleryCategory
+         * 
+         * @param {object} galleryCategory 
+         * @returns {Promis<object>}
+         */
+        add = async (galleryCategory: GalleryCategory) => await GalleryCategoryModel.create(galleryCategory); 
+        
+        /**
+         * count galleryCategorys
+         * 
+         * @returns {Promise<number>}
+         */
+        count = async (): Promise<number> => await GalleryCategoryModel.count(); 
+               
+        /**
+         * get all galleryCategorys
+         * 
+         * @returns {Promise<GalleryCategory[]>}
+         */
+        getAll = async () => await GalleryCategoryModel.find()
+        .populate('parentId')
+        .exec(); 
+        
+        /**
+         * get all galleryCategorys by parameters
+         * 
+         * @param {object} gridParameter 
+         * @returns {Promise<GalleryCategory[]>}
+         */
+        getAllByParams = async (gridParameter: GridParameter) : Promise<GalleryCategory[]> =>{
+            const { currentPage, pageSize, sortModel } = gridParameter;
+            const limitCount: number = (pageSize || AppConstant.PageSize);
+            const skipCount = (currentPage || 0) * limitCount;           
+            const sort = GridUtilityHelper.getSortObject(sortModel);
+            const list = await GalleryCategoryModel.find().sort(sort).skip(skipCount).limit(limitCount)
+            .populate('parentId')
+            .exec();
+            return list;
+        }  
+        
+        /**
+         * get galleryCategory by id
+         * 
+         * @param {string} id 
+         * @returns {Promise<GalleryCategory | null>}
+         */
+        getById = async (id: string): Promise<GalleryCategory | null> => await GalleryCategoryModel.findOne({ _id : id }); 
+        
+        /**
+         * update galleryCategory
+         * 
+         * @param {object} galleryCategory 
+         * @returns {Promise<object>}
+         */
+        update = async (galleryCategory: GalleryCategory) => {
+            return await GalleryCategoryModel.updateOne({ _id : galleryCategory._id },
+                { $set: { 
+                    parentId: galleryCategory.parentId,
+                    name: galleryCategory.name,
+                    description: galleryCategory.description,
+                    priority : galleryCategory.priority,
+                    iconCssClass : galleryCategory.iconCssClass,
+                    updatedBy: galleryCategory.updatedBy,
+                    updatedAt: galleryCategory.updatedAt
+                }});
+        }
+
+        /**
+         * toggle galleryCategory hidden status
+         * 
+         * @param {string} id 
+         * @param {boolean} toggleIsHidden
+         * @param {string} userId 
+         * @returns {Promise<object>}
+         */
+        toggleIsHidden = async (id: string, toggleIsHidden: boolean, userId: string) => {
+            return await GalleryCategoryModel.updateOne({_id: id}, 
+                { $set: { 
+                    isHidden: toggleIsHidden,
+                    updatedBy: userId,
+                    updatedAt: new Date()
+                }}); 
+        }
+        
+        /**
+         * toggle galleryCategory active status
+         * 
+         * @param {string} id 
+         * @param {boolean} toggleIsActive
+         * @param {string} userId 
+         * @returns {Promise<object>}
+         */
+        toggleIsActive = async (id: string, toggleIsActive: boolean, userId: string) => {
+            return await GalleryCategoryModel.updateOne({_id: id}, 
+                { $set: { 
+                    isActive: toggleIsActive,
+                    updatedBy: userId,
+                    updatedAt: new Date()
+                }}); 
+        }
+
+        /**
+         * delete galleryCategory by id
+         * 
+         * @param {string} id 
+         * @returns {Promise<object>}
+         */
+        delete = async (id: string) => await GalleryCategoryModel.deleteOne({ _id : id }); 
+    }
+
+
