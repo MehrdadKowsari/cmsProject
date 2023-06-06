3adabde861c4c31ce6e8bb0d61bc105752d8c220
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-06 20:24:52 +0330

    postRepository added.

---
+++ server/src/repositories/contentManagement/postRepository.ts
@@ -0,0 +1,94 @@
+import { GridParameter } from "src/dtos/shared/grid/gridPrameter";
+import PostModel, { Post } from "src/models/contentManagement/post";
+import GridUtilityHelper from "src/helpers/gridUtilityHelper";
+import AppConstant from "src/constants/appConstants";
+
+
+    export default class PostRepository{
+        /**
+         * add post
+         * 
+         * @param {object} post 
+         * @returns {Promis<object>}
+         */
+        add = async (post: Post) => await PostModel.create(post); 
+        
+        /**
+         * count posts
+         * 
+         * @returns {Promise<number>}
+         */
+        count = async (): Promise<number> => await PostModel.count(); 
+               
+        /**
+         * get all posts
+         * 
+         * @returns {Promise<Post[]>}
+         */
+        getAll = async () => await PostModel.find(); 
+        
+        /**
+         * get all posts by parameters
+         * 
+         * @param {object} gridParameter 
+         * @returns {Promise<Post[]>}
+         */
+        getAllByParams = async (gridParameter: GridParameter) : Promise<Post[]> =>{
+            const { currentPage, pageSize, sortModel } = gridParameter;
+            const limitCount: number = (pageSize || AppConstant.PageSize);
+            const skipCount = (currentPage || 0) * limitCount;           
+            const sort = GridUtilityHelper.getSortObject(sortModel);
+            const list = await PostModel.find().sort(sort).skip(skipCount).limit(limitCount);
+            return list;
+        }  
+        
+        /**
+         * get post by id
+         * 
+         * @param {string} id 
+         * @returns {Promise<Post | null>}
+         */
+        getById = async (id: string): Promise<Post | null> => await PostModel.findOne({ _id : id }); 
+        
+        /**
+         * update post
+         * 
+         * @param {object} post 
+         * @returns {Promise<object>}
+         */
+        update = async (post: Post) => {
+            return await PostModel.updateOne({ _id : post._id },
+                { $set: { 
+                    postCategoryId: post.postCategoryId,
+                    title: post.title,
+                    shortDescription: post.shortDescription,
+                    content: post.content,
+                    type : post.type,
+                    image : post.image,
+                    thumbnailImage : post.thumbnailImage,
+                    videoUrl : post.videoUrl,
+                    videoPoster : post.videoPoster,
+                    thumbnailVideoPoster : post.thumbnailVideoPoster,
+                    isCommentOpen : post.isCommentOpen,
+                    slugUrl : post.slugUrl,
+                    galleryId : post.galleryId,
+                    priority : post.priority,
+                    isFeatured : post.isFeatured,
+                    dateFrom : post.dateFrom,
+                    dateTo : post.dateTo,
+                    locale : post.locale,
+                    updatedBy: post.updatedBy,
+                    updatedAt: post.updatedAt
+                }});
+        }
+
+        /**
+         * delete post by id
+         * 
+         * @param {string} id 
+         * @returns {Promise<object>}
+         */
+        delete = async (id: string) => await PostModel.deleteOne({ _id : id }); 
+    }
+
+
