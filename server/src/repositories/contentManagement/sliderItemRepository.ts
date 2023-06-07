0d86b4ba456228b67e92a3455fc9210c9738a7cd
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-07 21:52:46 +0330

    sliderItemRepository added.

---
+++ server/src/repositories/contentManagement/sliderItemRepository.ts
@@ -0,0 +1,101 @@
+import { GridParameter } from "src/dtos/shared/grid/gridPrameter";
+import SliderItemModel, { SliderItem } from "src/models/contentManagement/sliderItem";
+import GridUtilityHelper from "src/helpers/gridUtilityHelper";
+import AppConstant from "src/constants/appConstants";
+
+
+    export default class SliderItemRepository{
+        /**
+         * add sliderItem
+         * 
+         * @param {object} sliderItem 
+         * @returns {Promis<object>}
+         */
+        add = async (sliderItem: SliderItem) => await SliderItemModel.create(sliderItem); 
+        
+        /**
+         * count sliderItems
+         * 
+         * @returns {Promise<number>}
+         */
+        count = async (): Promise<number> => await SliderItemModel.count(); 
+               
+        /**
+         * get all sliderItems
+         * 
+         * @returns {Promise<SliderItem[]>}
+         */
+        getAll = async () => await SliderItemModel.find(); 
+        
+        /**
+         * get all galleries by parameters
+         * 
+         * @param {object} gridParameter 
+         * @returns {Promise<SliderItem[]>}
+         */
+        getAllByParams = async (gridParameter: GridParameter) : Promise<SliderItem[]> =>{
+            const { currentPage, pageSize, sortModel } = gridParameter;
+            const limitCount: number = (pageSize || AppConstant.PageSize);
+            const skipCount = (currentPage || 0) * limitCount;           
+            const sort = GridUtilityHelper.getSortObject(sortModel);
+            const list = await SliderItemModel.find().sort(sort).skip(skipCount).limit(limitCount);
+            return list;
+        }  
+        
+        /**
+         * get sliderItem by id
+         * 
+         * @param {string} id 
+         * @returns {Promise<SliderItem | null>}
+         */
+        getById = async (id: string): Promise<SliderItem | null> => await SliderItemModel.findOne({ _id : id }); 
+        
+        /**
+         * update sliderItem
+         * 
+         * @param {object} sliderItem 
+         * @returns {Promise<object>}
+         */
+        update = async (sliderItem: SliderItem) => {
+            return await SliderItemModel.updateOne({ _id : sliderItem._id },
+                { $set: { 
+                    sliderId: sliderItem.sliderId,
+                    name: sliderItem.name,
+                    description: sliderItem.description,
+                    file : sliderItem.file,
+                    fileSavePath : sliderItem.fileSavePath,
+                    linkUrl : sliderItem.linkUrl,
+                    linkTarget : sliderItem.linkTarget,
+                    priority : sliderItem.priority,
+                    updatedBy: sliderItem.updatedBy,
+                    updatedAt: sliderItem.updatedAt
+                }});
+        }
+        
+        /**
+         * toggle slider item active status
+         * 
+         * @param {string} id 
+         * @param {boolean} toggleIsActive
+         * @param {string} userId 
+         * @returns {Promise<object>}
+         */
+        toggleIsActive = async (id: string, toggleIsActive: boolean, userId: string) => {
+            return await SliderItemModel.updateOne({_id: id}, 
+                { $set: { 
+                    isActive: toggleIsActive,
+                    updatedBy: userId,
+                    updatedAt: new Date()
+                }}); 
+        }
+
+        /**
+         * delete sliderItem by id
+         * 
+         * @param {string} id 
+         * @returns {Promise<object>}
+         */
+        delete = async (id: string) => await SliderItemModel.deleteOne({ _id : id }); 
+    }
+
+
