db01e854fa2e686f20a0e721eff1f462fe78fb69
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-12 20:01:17 +0330

    postController added.

---
+++ server/src/controllers/contentManagement/postController.ts
@@ -0,0 +1,120 @@
+import { Request, Response } from 'express';
+import { CRUDResultModel } from '../../models/shared/crud/crudResultModel';
+import { MethodResult } from '../../models/shared/crud/methodResult';
+import { CRUDResultEnum } from '../../models/shared/enums/crudResultEnum';
+import { StatusCodes } from 'http-status-codes';
+import PostService from '../../services/contentManagement/postService';
+import { autoInjectable } from 'tsyringe';
+import { GridParameter } from 'src/dtos/shared/grid/gridPrameter';
+import { UpdatePostDTO } from 'src/dtos/contentManagement/post/updatePostDTO';
+import LocalizerHelper from 'src/helpers/localizeHelper';
+
+@autoInjectable()
+export class PostController{
+    private _postService: PostService;
+    constructor(postService: PostService){
+        this._postService = postService;
+    }
+    /**
+     * add a post
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {object}
+     */
+    add = async (req: Request, res: Response) => {
+        try {
+            const requestResult = await this._postService.addPost(req.body, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get all posts
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getAll = async (req: Request, res: Response) => {
+        try {
+            const gridParameter: GridParameter = req.body;
+            const requestResult = await this._postService.getAll();
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get all posts by params
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getAllByParams = async (req: Request, res: Response) => {
+        try {
+            const gridParameter: GridParameter = req.body;
+            const requestResult = await this._postService.getAllByParams(gridParameter);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get post by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getById = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postService.getById(id);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+      
+    /**
+     * update post
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    update = async (req: Request, res: Response) => {
+        try {
+            const updatePostDTO: UpdatePostDTO = req.body;
+            const requestResult = await this._postService.update(updatePostDTO, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+            
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * delete post by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    delete = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postService.delete(id);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+}
