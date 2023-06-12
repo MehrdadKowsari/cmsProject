f9fa001157bf89c17be81cd145c58d8cf225c15c
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-12 23:03:44 +0330

    postImageController added.

---
+++ server/src/controllers/contentManagement/postImageController.ts
@@ -0,0 +1,120 @@
+import { Request, Response } from 'express';
+import { CRUDResultModel } from '../../models/shared/crud/crudResultModel';
+import { MethodResult } from '../../models/shared/crud/methodResult';
+import { CRUDResultEnum } from '../../models/shared/enums/crudResultEnum';
+import { StatusCodes } from 'http-status-codes';
+import PostImageService from '../../services/contentManagement/postImageService';
+import { autoInjectable } from 'tsyringe';
+import { GridParameter } from 'src/dtos/shared/grid/gridPrameter';
+import { UpdatePostImageDTO } from 'src/dtos/contentManagement/postImage/updatePostImageDTO';
+import LocalizerHelper from 'src/helpers/localizeHelper';
+
+@autoInjectable()
+export class PostImageController{
+    private _postImageService: PostImageService;
+    constructor(postImageService: PostImageService){
+        this._postImageService = postImageService;
+    }
+    /**
+     * add a postImage
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {object}
+     */
+    add = async (req: Request, res: Response) => {
+        try {
+            const requestResult = await this._postImageService.addPostImage(req.body, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get all postImages
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getAll = async (req: Request, res: Response) => {
+        try {
+            const gridParameter: GridParameter = req.body;
+            const requestResult = await this._postImageService.getAll();
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get all postImages by params
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getAllByParams = async (req: Request, res: Response) => {
+        try {
+            const gridParameter: GridParameter = req.body;
+            const requestResult = await this._postImageService.getAllByParams(gridParameter);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get postImage by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getById = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postImageService.getById(id);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+      
+    /**
+     * update postImage
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    update = async (req: Request, res: Response) => {
+        try {
+            const updatePostImageDTO: UpdatePostImageDTO = req.body;
+            const requestResult = await this._postImageService.update(updatePostImageDTO, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+            
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * delete postImage by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    delete = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postImageService.delete(id);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+}
