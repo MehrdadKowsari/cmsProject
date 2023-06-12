63053f49029cb1eff8cc3fd754f512a60281f2ab
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-12 20:04:40 +0330

    postCategoryController added.

---
+++ server/src/controllers/contentManagement/postCategoryController.ts
@@ -0,0 +1,137 @@
+import { Request, Response } from 'express';
+import { CRUDResultModel } from '../../models/shared/crud/crudResultModel';
+import { MethodResult } from '../../models/shared/crud/methodResult';
+import { CRUDResultEnum } from '../../models/shared/enums/crudResultEnum';
+import { StatusCodes } from 'http-status-codes';
+import PostCategoryService from '../../services/contentManagement/postCategoryService';
+import { autoInjectable } from 'tsyringe';
+import { GridParameter } from 'src/dtos/shared/grid/gridPrameter';
+import { UpdatePostCategoryDTO } from 'src/dtos/contentManagement/postCategory/updatePostCategoryDTO';
+import LocalizerHelper from 'src/helpers/localizeHelper';
+
+@autoInjectable()
+export class PostCategoryController{
+    private _postCategoryService: PostCategoryService;
+    constructor(postCategoryService: PostCategoryService){
+        this._postCategoryService = postCategoryService;
+    }
+    /**
+     * add a postCategory
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {object}
+     */
+    add = async (req: Request, res: Response) => {
+        try {
+            const requestResult = await this._postCategoryService.addPostCategory(req.body, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get all postCategorys
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getAll = async (req: Request, res: Response) => {
+        try {
+            const gridParameter: GridParameter = req.body;
+            const requestResult = await this._postCategoryService.getAll();
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get all postCategorys by params
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getAllByParams = async (req: Request, res: Response) => {
+        try {
+            const gridParameter: GridParameter = req.body;
+            const requestResult = await this._postCategoryService.getAllByParams(gridParameter);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+    /**
+     * get postCategory by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    getById = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postCategoryService.getById(id);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+      
+    /**
+     * update postCategory
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    update = async (req: Request, res: Response) => {
+        try {
+            const updatePostCategoryDTO: UpdatePostCategoryDTO = req.body;
+            const requestResult = await this._postCategoryService.update(updatePostCategoryDTO, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+            
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+     /**
+     * toggle postCategory active status by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    toggleActive = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postCategoryService.toggleActive(id, req.user?.id!);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+     
+    /**
+     * delete postCategory by id
+     * 
+     * @param {object} req 
+     * @param {object} res 
+     * @returns {Promise<object>} return
+     */
+    delete = async (req: Request, res: Response) => {
+        try {
+            const id = req.body;
+            const requestResult = await this._postCategoryService.delete(id);
+            return res.status(requestResult.statusCode).json(LocalizerHelper.localize(requestResult.methodResult, req));
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(LocalizerHelper.localize(new MethodResult(new CRUDResultModel(CRUDResultEnum.Error, 'unknownErrorHappened')), req));
+        }
+    }
+
+}
