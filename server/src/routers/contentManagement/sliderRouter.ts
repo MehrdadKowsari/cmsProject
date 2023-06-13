600af3b7e31229ef2a9d91e210c3e185d527a507
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-13 22:00:03 +0330

    routerValidation added.

---
+++ server/src/routers/contentManagement/sliderRouter.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { SliderController } from '../../controllers/contentManagement/sliderController';
+import {container} from 'tsyringe'; 
+import { addValidation, deleteValidation, getByIdValidation, toggleActiveValidation, updateValidation } from 'src/validations/contentManagement/sliderValidation';
+import permissionMiddleware from 'src/middleware/shared/permissionMiddleware';
+import { PermissionTypeEnum } from 'src/enums/security/permissionTypeEnum';
+import { PageTypeEnum } from 'src/enums/security/pageEnum';
+
+const sliderController = container.resolve(SliderController);
+const router = express.Router();
+
+router.post('/add', (req, res, next) => permissionMiddleware(req, res, next, PageTypeEnum.Slider, PermissionTypeEnum.Add), addValidation, sliderController.add);
+router.post('/getAll', sliderController.getAll);
+router.post('/getAllByParams', (req, res, next) => permissionMiddleware(req, res, next, PageTypeEnum.Slider, PermissionTypeEnum.View), sliderController.getAllByParams);
+router.post('/getById', (req, res, next) => permissionMiddleware(req, res, next, PageTypeEnum.Slider, PermissionTypeEnum.View), getByIdValidation, sliderController.getById);
+router.post('/update', (req, res, next) => permissionMiddleware(req, res, next, PageTypeEnum.Slider, PermissionTypeEnum.Update), updateValidation, sliderController.update);
+router.post('/toggleActive', (req, res, next) => permissionMiddleware(req, res, next, PageTypeEnum.Slider, PermissionTypeEnum.ToggleActive), toggleActiveValidation, sliderController.toggleActive);
+router.post('/delete', (req, res, next) => permissionMiddleware(req, res, next, PageTypeEnum.Slider, PermissionTypeEnum.Delete), deleteValidation, sliderController.delete);
+
+export default router;
