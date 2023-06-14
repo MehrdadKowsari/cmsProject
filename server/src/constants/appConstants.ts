a94af2e11212467bcac74c08ebb476c9e0dbae18
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-14 20:45:00 +0330

    some constants added to appConstants.

--- server/src/constants/appConstants.ts
+++ server/src/constants/appConstants.ts
@@ -8,5 +8,8 @@ export default class AppConstant {
     public static readonly UsernameMaxLenght:number = 50;
     public static readonly PasswordMinLenght:number = 6;
     public static readonly NameMaxLenght:number = 200;
+    public static readonly TitleMaxLenght:number = 1000;
     public static readonly DescriptionMaxLenght:number = 500;
+    public static readonly AllowedFileExtensionMaxLength:number = 200;
+    public static readonly SlugUrlMaxLength:number = 200;
 }
