7490b04caa8440e48e390caf62bbc5be1e8096e2
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-15 21:56:57 +0330

    some enteries adde to translation and appConstant.

--- server/src/constants/appConstants.ts
+++ server/src/constants/appConstants.ts
@@ -13,6 +13,10 @@ export default class AppConstant {
     public static readonly AllowedFileExtensionMaxLength:number = 200;
     public static readonly SlugUrlMaxLength:number = 2000;
     public static readonly UrlMaxLength:number = 2000;
+    public static readonly EmailMaxLength:number = 200;
+    public static readonly FullNameMaxLength:number = 200;
+    public static readonly WebsiteMaxLength:number = 200;
+    public static readonly IPMaxLength:number = 200;
     public static readonly FileSavePathMaxLength:number = 5000;
     public static readonly FileExtensionMaxLength:number = 100;
     public static readonly SectionNameMaxLength:number = 200;
