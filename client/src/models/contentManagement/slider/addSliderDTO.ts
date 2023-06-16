32ac4aa7dfb5235966f4787fb5075a8233e9c6d6
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-16 16:31:57 +0330

    slider model dtos in client-side.

---
+++ client/src/models/contentManagement/slider/addSliderDTO.ts
@@ -0,0 +1,15 @@
+import { SliderTypeEnum } from "src/enums/contentManagement/sliderTypeEnum";
+
+export interface AddSliderDTO{
+  articleCategoryId?: string | null;
+  galleryId?: string | null;
+  name: string;
+  description: string | null;
+  type: SliderTypeEnum;
+  params: string | null;
+  sectionName: string | null;
+  allowedFileExtension: string | null;
+  priority: number | number;
+  locale: string | null;
+}
+
---
+++ client/src/models/contentManagement/slider/sliderDTO.ts
@@ -0,0 +1,24 @@
+import { SliderTypeEnum } from "src/enums/contentManagement/sliderTypeEnum";
+import { BaseModel } from "../../../models/shared/base/baseModel";
+
+export interface SliderDTO extends BaseModel {
+    articleCategoryId?: string | null;
+    articleCategoryName?: string | null;
+    galleryId?: string | null;
+    galleryName?: string | null;
+    name: string;
+    description: string;
+    type: SliderTypeEnum;
+    typeName: string;
+    params: string;
+    sectionName: string;
+    allowedFileExtension: string;
+    priority: number;
+    isActive: boolean;
+    locale: string;
+    createdBy?: string;
+    createdAt?: Date;
+    updatedBy?: string;
+    updatedAt?: Date;
+}
+
