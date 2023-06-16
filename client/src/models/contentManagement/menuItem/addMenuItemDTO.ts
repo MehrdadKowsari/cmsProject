4a8e5b52da1fea2a2a2c470dff15426adaa548df
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-16 16:25:54 +0330

    menuItem model dtos in client-side.

---
+++ client/src/models/contentManagement/menuItem/addMenuItemDTO.ts
@@ -0,0 +1,20 @@
+import { MenuItemType } from "src/enums/contentManagement/menuItemTypeEnum";
+
+export interface AddMenuItemDTO{
+  menuId: string;
+  parentId: string | null;
+  name: string | null;
+  description: string | null;
+  image: string | null;
+  imageSavePath: string | null;
+  level: number | null;
+  url: string;
+  slugUrl: string | null;
+  target: string;
+  rel: string | null;
+  iconCssClass: string | null;
+  iconSavePath: string | null;
+  type: MenuItemType,
+  priority: number | null;
+}
+
