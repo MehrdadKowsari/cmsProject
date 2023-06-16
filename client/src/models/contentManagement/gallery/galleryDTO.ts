91fbbc09d5293c9c2ec3b10c79cdc8d041dc819c
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-16 16:24:37 +0330

    gallery model dtos in client-side.

---
+++ client/src/models/contentManagement/gallery/galleryDTO.ts
@@ -0,0 +1,27 @@
+import { BaseModel } from "../../../models/shared/base/baseModel";
+import { GalleryTypeEnum } from "src/enums/contentManagement/galleryTypeEnum";
+
+export interface GalleryDTO extends BaseModel {
+    galleryCategoryId: string;
+    galleryCategoryName: string;
+    name: string;
+    description: string | null;
+    params: string | null;
+    type: GalleryTypeEnum;
+    typeName: string | null;
+    image: string | null;
+    thumbnailImage: string | null;
+    slugUrl: string | null;
+    allowedFileExtension: string | null;
+    visitNumber: number;
+    likeCount: number;
+    dislikeCount: number;
+    priority: number | number;
+    isActive: boolean;
+    locale: string;
+    createdBy?: string;
+    createdAt?: Date;
+    updatedBy?: string;
+    updatedAt?: Date;
+}
+
---
+++ client/src/models/contentManagement/gallery/updateGalleryDTO.ts
@@ -0,0 +1,17 @@
+import { BaseModel } from "../../../models/shared/base/baseModel";
+import { GalleryTypeEnum } from "src/enums/contentManagement/galleryTypeEnum";
+
+export interface UpdateGalleryDTO extends BaseModel {
+  galleryCategoryId: string;
+  name: string;
+  description: string | null;
+  params: string | null;
+  type: GalleryTypeEnum;
+  image: string | null;
+  thumbnailImage: string | null;
+  slugUrl: string | null;
+  allowedFileExtension: string | null;
+  priority: number | number;
+  locale: string | null;
+}
+
