3bd4a9c2b9d8e56bb257b2d9389c5bdb7a5d2cff
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-16 20:53:14 +0330

    verical menu entries updated.

--- client/src/navigation/vertical/index.ts
+++ client/src/navigation/vertical/index.ts
@@ -12,6 +12,11 @@ const navigation = (): VerticalNavItemsType => {
       sectionTitle: 'Content Management'
     },
     {
+      title: 'Tags',
+      icon: 'tag',
+      path: '/admin/contentManagement/tag'
+    },
+    {
       sectionTitle: 'Security'
     },
     {
