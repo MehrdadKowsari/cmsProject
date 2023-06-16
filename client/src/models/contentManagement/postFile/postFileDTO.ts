3216263516ca7a82440419cf8be1172de2c0b5f2
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-16 16:30:14 +0330

    postFile model dtos in client-side.

---
+++ client/src/models/contentManagement/postFile/postFileDTO.ts
@@ -0,0 +1,18 @@
+import { BaseModel } from "../../../models/shared/base/baseModel";
+
+export interface PostFileDTO extends BaseModel {
+  postId: string;
+  postTitle: string | null;
+  name: string;
+  description: string | null;
+  file: string | null;
+  fileSavePath: string | null;
+  fileExtension: string | null;
+  fileSize: number | null;
+  downloadCount: number;
+  priority: number | null;
+  createdBy?: string;
+  createdAt?: Date;
+  updatedBy?: string;
+  updatedAt?: Date;
+}
