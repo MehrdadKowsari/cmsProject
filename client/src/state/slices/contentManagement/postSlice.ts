ed65823849a7a01c9257b0eb84731f9571fe2f5f
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-18 21:32:33 +0330

    postSlice added.

---
+++ client/src/state/slices/contentManagement/postSlice.ts
@@ -0,0 +1,188 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { AxiosError } from 'axios';
+import axios from 'src/api/axios';
+import { AddPostDTO } from 'src/models/contentManagement/post/addPostDTO';
+import { UpdatePostDTO } from 'src/models/contentManagement/post/updatePostDTO';
+import { PostDTO } from 'src/models/contentManagement/post/postDTO';
+import { IntialState } from 'src/state/interfaces/intialState';
+import { GridParameter } from 'src/models/shared/grid/gridPrameter';
+
+const API_URL: string = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/post`;
+
+export const add = createAsyncThunk(
+  "posts/add", 
+  async (post: AddPostDTO, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/add`, post)
+      return data?.result;
+    } catch (err) {
+        const error= err as AxiosError;
+        return rejectWithValue(error.message);
+    }
+});
+
+export const getAllPosts = createAsyncThunk(
+  "posts/getAll", 
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/getAll`);
+      return data?.result;
+    } catch (err) {
+      const error= err as AxiosError;
+      return rejectWithValue(error.message);
+    }
+});
+
+export const getAllByParams = createAsyncThunk(
+  "posts/getAllByParams", 
+  async (gridParameter: GridParameter, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/getAllByParams`, gridParameter);
+      return data?.result;
+    } catch (err) {
+      const error= err as AxiosError;
+      return rejectWithValue(error.message);
+    }
+});
+
+export const getById = createAsyncThunk(
+  'posts/getById',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/getById`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+export const update = createAsyncThunk(
+  "posts/update", 
+  async (post: UpdatePostDTO, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/update`, post)
+      return data?.result;
+    } catch (err) {
+        const error= err as AxiosError;
+        return rejectWithValue(error.message);
+    }
+});
+
+export const remove = createAsyncThunk(
+  'posts/delete',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/delete`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+
+interface PostState extends IntialState {
+  posts: PostDTO[] | null,
+  post: PostDTO | null
+}
+
+const initialState: PostState = {
+    posts: null,
+    post: null,
+    isLoading: false,
+    hasError: false,
+    totalCount: 0,
+    currentPage: 1,
+    numberOfPages: null,
+    error: ''
+}
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+          .addCase(add.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(add.fulfilled, (state, { payload }) => {
+            state.post = payload;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(add.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+
+          .addCase(getAllByParams.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(getAllByParams.fulfilled, (state, { payload }) => {
+            state.posts = payload.rows;
+            state.totalCount = payload.totalCount;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(getAllByParams.rejected, (state, { payload }) => {
+            state.posts = null;
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(getById.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(getById.fulfilled, (state, { payload }) => {
+            state.post = payload;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(getById.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(update.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(update.fulfilled, (state, { payload }) => {
+            state.post = payload;
+            state.isLoading = false;
+            state.hasError = false;
+            state.error = <string>payload;
+          })
+          .addCase(update.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(remove.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(remove.fulfilled, (state) => {
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(remove.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+    }
+});
+
+export default postSlice.reducer;
