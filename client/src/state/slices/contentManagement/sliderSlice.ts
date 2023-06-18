90f564536c10feb505fc084e5e8e72ed5de3cbf1
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-18 21:33:18 +0330

    sliderSlice added.

---
+++ client/src/state/slices/contentManagement/sliderSlice.ts
@@ -0,0 +1,215 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { AxiosError } from 'axios';
+import axios from 'src/api/axios';
+import { AddSliderDTO } from 'src/models/contentManagement/slider/addSliderDTO';
+import { UpdateSliderDTO } from 'src/models/contentManagement/slider/updateSliderDTO';
+import { SliderDTO } from 'src/models/contentManagement/slider/sliderDTO';
+import { IntialState } from 'src/state/interfaces/intialState';
+import { GridParameter } from 'src/models/shared/grid/gridPrameter';
+
+const API_URL: string = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/slider`;
+
+export const add = createAsyncThunk(
+  "sliders/add", 
+  async (slider: AddSliderDTO, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/add`, slider)
+      return data?.result;
+    } catch (err) {
+        const error= err as AxiosError;
+        return rejectWithValue(error.message);
+    }
+});
+
+export const getAllGalleryCategories = createAsyncThunk(
+  "sliders/getAll", 
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/getAll`);
+      return data?.result;
+    } catch (err) {
+      const error= err as AxiosError;
+      return rejectWithValue(error.message);
+    }
+});
+
+export const getAllByParams = createAsyncThunk(
+  "sliders/getAllByParams", 
+  async (gridParameter: GridParameter, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/getAllByParams`, gridParameter);
+      return data?.result;
+    } catch (err) {
+      const error= err as AxiosError;
+      return rejectWithValue(error.message);
+    }
+});
+
+export const getById = createAsyncThunk(
+  'sliders/getById',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/getById`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+export const update = createAsyncThunk(
+  "sliders/update", 
+  async (slider: UpdateSliderDTO, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/update`, slider)
+      return data?.result;
+    } catch (err) {
+        const error= err as AxiosError;
+        return rejectWithValue(error.message);
+    }
+});
+
+export const toggleActive = createAsyncThunk(
+  'sliders/toggleActive',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/toggleActive`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+export const remove = createAsyncThunk(
+  'sliders/delete',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/delete`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+
+interface SliderState extends IntialState {
+  sliders: SliderDTO[] | null,
+  slider: SliderDTO | null
+}
+
+const initialState: SliderState = {
+    sliders: null,
+    slider: null,
+    isLoading: false,
+    hasError: false,
+    totalCount: 0,
+    currentPage: 1,
+    numberOfPages: null,
+    error: ''
+}
+
+const sliderSlice = createSlice({
+    name: 'sliders',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+          .addCase(add.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(add.fulfilled, (state, { payload }) => {
+            state.slider = payload;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(add.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+
+          .addCase(getAllByParams.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(getAllByParams.fulfilled, (state, { payload }) => {
+            state.sliders = payload.rows;
+            state.totalCount = payload.totalCount;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(getAllByParams.rejected, (state, { payload }) => {
+            state.sliders = null;
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(getById.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(getById.fulfilled, (state, { payload }) => {
+            state.slider = payload;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(getById.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(update.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(update.fulfilled, (state, { payload }) => {
+            state.slider = payload;
+            state.isLoading = false;
+            state.hasError = false;
+            state.error = <string>payload;
+          })
+          .addCase(update.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(toggleActive.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(toggleActive.fulfilled, (state) => {
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(toggleActive.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(remove.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(remove.fulfilled, (state) => {
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(remove.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+    }
+});
+
+export default sliderSlice.reducer;
