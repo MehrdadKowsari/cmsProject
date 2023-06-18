9030ffbd4d5ee5f9cfe4635d865881a16a20e1b8
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-18 21:31:10 +0330

    galleryFileSlice added.

---
+++ client/src/state/slices/contentManagement/galleryFileSlice.ts
@@ -0,0 +1,215 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { AxiosError } from 'axios';
+import axios from 'src/api/axios';
+import { AddGalleryFileDTO } from 'src/models/contentManagement/galleryFile/addGalleryFileDTO';
+import { UpdateGalleryFileDTO } from 'src/models/contentManagement/galleryFile/updateGalleryFileDTO';
+import { GalleryFileDTO } from 'src/models/contentManagement/galleryFile/galleryFileDTO';
+import { IntialState } from 'src/state/interfaces/intialState';
+import { GridParameter } from 'src/models/shared/grid/gridPrameter';
+
+const API_URL: string = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/galleryFile`;
+
+export const add = createAsyncThunk(
+  "galleryFiles/add", 
+  async (galleryFile: AddGalleryFileDTO, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/add`, galleryFile)
+      return data?.result;
+    } catch (err) {
+        const error= err as AxiosError;
+        return rejectWithValue(error.message);
+    }
+});
+
+export const getAllGalleryCategories = createAsyncThunk(
+  "galleryFiles/getAll", 
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/getAll`);
+      return data?.result;
+    } catch (err) {
+      const error= err as AxiosError;
+      return rejectWithValue(error.message);
+    }
+});
+
+export const getAllByParams = createAsyncThunk(
+  "galleryFiles/getAllByParams", 
+  async (gridParameter: GridParameter, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/getAllByParams`, gridParameter);
+      return data?.result;
+    } catch (err) {
+      const error= err as AxiosError;
+      return rejectWithValue(error.message);
+    }
+});
+
+export const getById = createAsyncThunk(
+  'galleryFiles/getById',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/getById`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+export const update = createAsyncThunk(
+  "galleryFiles/update", 
+  async (galleryFile: UpdateGalleryFileDTO, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${API_URL}/update`, galleryFile)
+      return data?.result;
+    } catch (err) {
+        const error= err as AxiosError;
+        return rejectWithValue(error.message);
+    }
+});
+
+export const toggleActive = createAsyncThunk(
+  'galleryFiles/toggleActive',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/toggleActive`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+export const remove = createAsyncThunk(
+  'galleryFiles/delete',
+  async (id: string | number, { rejectWithValue }) => {
+      try {
+          const { data } = await axios.post(`${API_URL}/delete`, id);
+          return data?.result;
+      } catch (err) {
+          const error= err as AxiosError;
+          return rejectWithValue(error.message);
+      }
+  }
+)
+
+
+interface GalleryFileState extends IntialState {
+  galleryFiles: GalleryFileDTO[] | null,
+  galleryFile: GalleryFileDTO | null
+}
+
+const initialState: GalleryFileState = {
+    galleryFiles: null,
+    galleryFile: null,
+    isLoading: false,
+    hasError: false,
+    totalCount: 0,
+    currentPage: 1,
+    numberOfPages: null,
+    error: ''
+}
+
+const galleryFileSlice = createSlice({
+    name: 'galleryFiles',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+          .addCase(add.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(add.fulfilled, (state, { payload }) => {
+            state.galleryFile = payload;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(add.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+
+          .addCase(getAllByParams.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(getAllByParams.fulfilled, (state, { payload }) => {
+            state.galleryFiles = payload.rows;
+            state.totalCount = payload.totalCount;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(getAllByParams.rejected, (state, { payload }) => {
+            state.galleryFiles = null;
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(getById.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(getById.fulfilled, (state, { payload }) => {
+            state.galleryFile = payload;
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(getById.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(update.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(update.fulfilled, (state, { payload }) => {
+            state.galleryFile = payload;
+            state.isLoading = false;
+            state.hasError = false;
+            state.error = <string>payload;
+          })
+          .addCase(update.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(toggleActive.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(toggleActive.fulfilled, (state) => {
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(toggleActive.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+          
+          .addCase(remove.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+          })
+          .addCase(remove.fulfilled, (state) => {
+            state.isLoading = false;
+            state.hasError = false;
+          })
+          .addCase(remove.rejected, (state, { payload }) => {
+            state.hasError = true;
+            state.isLoading = false;
+            state.error = <string>payload;
+          })
+    }
+});
+
+export default galleryFileSlice.reducer;
