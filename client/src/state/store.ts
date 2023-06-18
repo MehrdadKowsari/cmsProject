26e565cc84d44723d30b77cad0d14f5a446c0003
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-06-18 21:35:02 +0330

    store to adding new contentMnangent slices updated.

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -9,11 +9,25 @@ import rolePagePermissionReducer from './slices/rolePagePermissionSlice';
 import pageReducer from './slices/pageSlice';
 import userRoleReducer from './slices/userRoleSlice';
 import postCategoryReducer from './slices/contentManagement/postCategorySlice';
+import postReducer from './slices/contentManagement/postSlice';
+import postCommentReducer from './slices/contentManagement/postCommentSlice';
+import postFileReducer from './slices/contentManagement/postFileSlice';
+import postImageReducer from './slices/contentManagement/postImageSlice';
+import postTagReducer from './slices/contentManagement/postTagSlice';
+import relatedPostReducer from './slices/contentManagement/relatedPostSlice';
 import galleryCategoryReducer from './slices/contentManagement/galleryCategorySlice';
+import galleryReducer from './slices/contentManagement/gallerySlice';
+import galleryFileReducer from './slices/contentManagement/galleryFileSlice';
 import tagReducer from './slices/contentManagement/tagSlice';
+import sliderReducer from './slices/contentManagement/sliderSlice';
+import sliderItemReducer from './slices/contentManagement/sliderItemSlice';
+import menuReducer from './slices/contentManagement/menuSlice';
+import menuItemReducer from './slices/contentManagement/menuItemSlice';
 
 export const store = configureStore({
     reducer: {
+        confirm: confirmReducer,
+        
         auth: authReducer,
         user: userReducer,
         role: roleReducer,
@@ -23,10 +37,21 @@ export const store = configureStore({
         rolePagePermission: rolePagePermissionReducer,
         userRole: userRoleReducer,
         
-        postCategory: postCategoryReducer,
         galleryCategory: galleryCategoryReducer,
+        gallery: galleryReducer,
+        galleryFile: galleryFileReducer,
+        postCategory: postCategoryReducer,
+        post: postReducer,
+        postComment: postCommentReducer,
+        postFile: postFileReducer,
+        postImage: postImageReducer,
+        postTag: postTagReducer,
+        relatedPost: relatedPostReducer,
         tag: tagReducer,
-        confirm: confirmReducer
+        slider: sliderReducer,
+        sliderItem: sliderItemReducer,
+        menu: menuReducer,
+        menuItem: menuItemReducer,
     },
     devTools: true
 });
