c3d9cde50d6cf27d238702783ca5b9632dbf11e6
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-07 14:24:55 +0330

    dataGridPagination component added.

---
+++ client/src/components/DataGridPagination/DataGridPagination.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {
+  gridPageCountSelector,
+  GridPagination,
+  useGridApiContext,
+  useGridSelector
+} from '@mui/x-data-grid';
+import MuiPagination from '@mui/material/Pagination';
+import { TablePaginationProps } from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
+import RefreshIcon from '@mui/icons-material/Refresh'
+
+declare module '@mui/x-data-grid' {
+    interface PaginationPropsOverrides{
+        onRefreshButtonClick: () => void
+    }
+}
+function Pagination({
+  page,
+  onPageChange,
+  className,
+}: TablePaginationProps ) {
+  const apiRef = useGridApiContext();
+  const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+
+  return (
+    <MuiPagination
+      color="primary"
+      className={className}
+      count={pageCount}
+      page={page + 1}
+      onChange={(event, newPage) => {
+        onPageChange(event as any, newPage - 1);
+      }}
+    />
+  );
+}
+
+const DataGridPagination = (props: any) => {
+  return (<>
+    <GridPagination ActionsComponent={Pagination} {...props} /> <IconButton onClick={props.onRefreshButtonClick}><RefreshIcon/></IconButton>
+  </>)
+}
+
+export default DataGridPagination;
+
