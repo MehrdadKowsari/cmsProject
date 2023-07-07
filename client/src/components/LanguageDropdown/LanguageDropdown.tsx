c4864d845bfe2b528bc0765da841fb01b75eb391
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-07 14:24:33 +0330

    lnguageDropdown component added.

---
+++ client/src/components/LanguageDropdown/LanguageDropdown.tsx
@@ -0,0 +1,52 @@
+import MenuItem from '@mui/material/MenuItem';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { LanguageLabbelMapping, LanguageCodeEnum } from 'src/models/shared/enums/languageCodeEnum';
+import { TextValueDTO } from 'src/models/shared/list/textValueDTO';
+
+type LanguageDropdownProps = TextFieldProps & {
+
+}
+
+const LanguageDropdown = (props: LanguageDropdownProps) => {
+    const [languages, setLanguages] = useState<TextValueDTO[]>([]);
+    const { t } = useTranslation(['common']);
+    useEffect(() => {
+        getLanguageList();
+      
+    }, []);
+
+    const getLanguageList = () => {
+        const languageSelectList: TextValueDTO[] = Object.values(LanguageCodeEnum).filter(p => typeof p === 'string').map(p => ({
+                text: t(LanguageLabbelMapping[p]),
+                value: p.toString()
+            } as TextValueDTO));
+            
+            setLanguages(languageSelectList);
+    }
+
+    return(
+        <>
+            <TextField
+                  select 
+                  fullWidth
+                  id={props.id}
+                  name={props.name}
+                  label={props.label}
+                  value={props.value} 
+                  onChange={props.onChange}
+                  onBlur={props.onBlur}
+                  error={props.error}
+                  helperText={props.helperText}>
+                    {languages.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.text}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+        </>
+    )
+}
+
+export default LanguageDropdown;
---
+++ client/src/models/shared/enums/languageCodeEnum.ts
@@ -0,0 +1,9 @@
+export enum LanguageCodeEnum {
+    Persian = 'fa',
+    English = 'en'
+}
+
+export const LanguageLabbelMapping: Record<LanguageCodeEnum, string> = {
+    [LanguageCodeEnum.Persian]: 'persian',
+    [LanguageCodeEnum.English]: 'english'
+}
