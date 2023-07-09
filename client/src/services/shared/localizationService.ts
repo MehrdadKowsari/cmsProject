e438a5455116040716bf66276a07866c9fc6047a
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-09 19:13:54 +0330

    languageCodeEnum added into localizationService.

--- client/src/services/shared/localizationService.ts
+++ client/src/services/shared/localizationService.ts
@@ -1,15 +1,33 @@
 import moment from 'jalali-moment';
-import { useRouter } from 'next/router'
+import { NextRouter } from 'next/router'
+import ApplicationParams from 'src/constants/applicationParams';
+import { LanguageCodeEnum } from 'src/models/shared/enums/languageCodeEnum';
 
 class LocalizationService {
+    
+    getLanguageCodeByRouter(router: NextRouter){
+        return router?.locale || ApplicationParams.DefaultLanguageCode;
+    }
+
     getLocalDate(date: string, locale: string): string | null{
         if (!date) {
             return null;
         }
-        if (locale === 'fa') {
+        if (locale === LanguageCodeEnum.Persian) {
             return moment(date).format('jYYYY/jMM/jDD')
         } else {
             return moment(date).format('DD/MM/YYYY')
+        }
+    }
+    
+    getLocalDateTime(dateTime: string, locale: string): string | null{
+        if (!dateTime) {
+            return null;
+        }
+        if (locale === LanguageCodeEnum.Persian) {
+            return moment(dateTime).format('jYYYY/jMM/jDD HH:MM')
+        } else {
+            return moment(dateTime).format('DD/MM/YYYY HH:MM')
         }
     }
 }
