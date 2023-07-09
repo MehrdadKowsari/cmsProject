9af2c86ffc9d62b1607e275b5e64abaf67237a77
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-09 22:08:54 +0330

    galleryTypeEnumlabelMapping added into galleryTypeEnum.

--- client/src/models/contentManagement/enums/galleryTypeEnum.ts
+++ client/src/models/contentManagement/enums/galleryTypeEnum.ts
@@ -3,3 +3,10 @@ export enum GalleryTypeEnum {
     Video = 2,
     Audio = 3
 }
+
+export const GalleryTypeEnumLabelMapping: Record<GalleryTypeEnum, string> = {
+    [GalleryTypeEnum.Image]: 'image',
+    [GalleryTypeEnum.Video]: 'video',
+    [GalleryTypeEnum.Audio]: 'audio',
+
+}
