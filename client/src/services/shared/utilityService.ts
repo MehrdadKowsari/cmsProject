3551c7e1acf3e7652f8cf00dd136157116604bf7
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-14 12:37:09 +0330

    utilityService getTextValueListByEnum updated.

--- client/src/services/shared/utilityService.ts
+++ client/src/services/shared/utilityService.ts
@@ -3,8 +3,8 @@ import { TextValueDTO } from "src/models/shared/list/textValueDTO";
 class UtilityService {
     getTextValueListByEnum<T>(enumType: object, enumLabelMapping: any): TextValueDTO[] {
         const pageTypes: TextValueDTO[] = Object.values(enumType).filter(p => typeof p === 'number').map(p => (<TextValueDTO>{
-            text: p,
-            value: enumLabelMapping[p]
+            text: enumLabelMapping[p],
+            value: p.toString()
         }));
         return pageTypes;
     }
