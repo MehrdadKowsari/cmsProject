82a4ee16abb2271bd1e04ead06f35ada5c224870
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-16 21:25:55 +0330

    RTE component based on CKEditpr package updated.

--- client/src/components/RichTextEditor/RichTextEditor.tsx
+++ client/src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,31 +1,59 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from 'draft-js';
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useRouter } from "next/router";
+import ApplicationParams from "src/constants/applicationParams";
+import localizationService from "src/services/shared/localizationService";
+
 interface CKeditorProps {
   onChange: (data: string) => void;
   editorLoaded: boolean;
   name: string;
-  value: string;
+  value: string | null;
+  locale: string
 }
 
-export default function RichTextEditor({
+export default function CKeditor({
   onChange,
   editorLoaded,
   name,
+  locale,
   value,
 }: CKeditorProps) {
-    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
-
+  const router = useRouter();
+  const editorRef = useRef<{ CKEditor: typeof CKEditor; ClassicEditor: typeof ClassicEditor }>();
+  useEffect(() => {
+    editorRef.current = {
+      CKEditor: require("@ckeditor/ckeditor5-react").CKEditor,
+      ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),
+    };
+  }, []);
   return (
     <>
-      <Editor
-        editorStyle={{ height: '350px' }}
-        textAlignment='right'
-        editorState={editorState}
-        onEditorStateChange={setEditorState}
+      
+        <CKEditor
+          editor={ClassicEditor}
+          data={value}
+          onChange={(event: any, editor: any) => {
+            const data = editor.getData();
+            onChange(data);
+          }}
+          config={{
+           language: locale,           
+          }}
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            // console.log("Editor is ready to use!", editor);
+            editor.editing.view.change((writer) => {
+            writer.setStyle(
+                "height",
+                "200px",
+                editor.editing.view.document.getRoot()!
+            );
+            });
+        }}
         />
-
+      
     </>
   );
 }
