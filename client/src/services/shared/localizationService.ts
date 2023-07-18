692174027ac3a0743aeea5470492e86af552a83c
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-18 23:53:03 +0330

    localizationService parseDateTime method added.

--- client/src/services/shared/localizationService.ts
+++ client/src/services/shared/localizationService.ts
@@ -1,3 +1,5 @@
+import { parseISO as parseISOJalali } from 'date-fns-jalali';
+import { parseISO } from 'date-fns';
 import moment from 'jalali-moment';
 import { NextRouter } from 'next/router'
 import ApplicationParams from 'src/constants/applicationParams';
@@ -9,18 +11,22 @@ class LocalizationService {
         return router?.locale || ApplicationParams.DefaultLanguageCode;
     }
 
-    getLocalDate(date: string, locale: string): string | null{
-        if (!date) {
+    getLocalDate(date: string | null | undefined, locale?: string | null): string | null{
+        try {
+            if (!date) {
+                return null;
+            }
+            if (locale === LanguageCodeEnum.Persian) {
+                return moment(date).format('jYYYY/jMM/jDD')
+            } else {
+                return moment(date).format('DD/MM/YYYY')
+            }
+        } catch (error) {
             return null;
-        }
-        if (locale === LanguageCodeEnum.Persian) {
-            return moment(date).format('jYYYY/jMM/jDD')
-        } else {
-            return moment(date).format('DD/MM/YYYY')
         }
     }
     
-    getLocalDateTime(dateTime: string, locale: string | null | undefined): string | null{
+    getLocalDateTime(dateTime: string | null | undefined, locale?: string | null): string | null{
         if (!dateTime) {
             return null;
         }
@@ -29,6 +35,18 @@ class LocalizationService {
         } else {
             return moment(dateTime).format('DD/MM/YYYY HH:MM')
         }
+    }
+
+    parseDateTime(dateTime: string | null | Date, locale?: string | null): Date | null{
+       try {
+        if (!dateTime) {
+            return null;
+        }
+        const formattedDate = locale === LanguageCodeEnum.Persian ? parseISOJalali(dateTime.toString()) : parseISO(dateTime.toString())
+        return formattedDate;
+       } catch (error) {
+        return null;
+       }
     }
 }
 
