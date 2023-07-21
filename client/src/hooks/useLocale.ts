128540cf7b1ab4a38e7caf20d0c0d98f01863814
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-22 00:07:20 +0330

    useLocale hook added.

--- client/src/hooks/useLocale.ts
+++ client/src/hooks/useLocale.ts
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
+import ApplicationParams from "src/constants/applicationParams";
 
 const useLocale = () => {
     const router  = useRouter();
     const getLocale = () => {
-        return router.locale;
+        return router.locale ?? ApplicationParams.DefaultLanguageCode;
     }
 
     return { getLocale };
