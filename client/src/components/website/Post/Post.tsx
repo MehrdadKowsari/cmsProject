e1322b53fb75f7cddb35aef72be3a40743c0b237
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-07-23 22:45:12 +0330

    post component added.

---
+++ client/src/components/website/Post/Post.tsx
@@ -0,0 +1,75 @@
+import { format } from "date-fns";
+import Link from "next/link";
+import React from "react";
+import { Routes } from "../../../config/routes";
+import { ClockIcon } from "../../../svg";
+import {
+  DateText,
+  DateWrapper,
+  ImageWrapper,
+  PostBody,
+  PostFooter,
+  PostText,
+  PostTitle,
+  PostWrapper,
+  StyledImage,
+  StyledLink,
+  StyledImagePlaceholder,
+  PostTextWrapper,
+} from "./styles";
+import { PostDTO } from "src/models/contentManagement/post/postDTO";
+import localizationService from "src/services/shared/localizationService";
+import useLocale from "src/hooks/useLocale";
+
+type Props = {
+  post: PostDTO;
+};
+
+const Post: React.FC<Props> = ({ post }) => {
+  const { getLocale } = useLocale();
+  const locale = getLocale();
+  return (
+    <PostWrapper>
+      <ImageWrapper>
+        <Link href={`${Routes.Blog}/${post.slugUrl}/`} passHref>
+          <StyledLink>
+            {post.image ? (
+              <StyledImage
+                priority
+                unoptimized
+                src={post.image!}
+                alt={post.title}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="50% 50%"
+              />
+            ) : (
+              <StyledImagePlaceholder />
+            )}
+          </StyledLink>
+        </Link>
+      </ImageWrapper>
+      <PostBody>
+        <Link href={`${Routes.Blog}/${post.slugUrl}/`} passHref>
+          <StyledLink>
+            <PostTitle>{post.title}</PostTitle>
+          </StyledLink>
+        </Link>
+        <PostTextWrapper>
+          <PostText>{post.shortDescription}</PostText>
+        </PostTextWrapper>
+
+        <PostFooter>
+          <DateWrapper>
+            <ClockIcon />
+            <DateText>
+              {localizationService.getLocalDateTime(post.createdAt?.toString(), locale)}
+            </DateText>
+          </DateWrapper>
+        </PostFooter>
+      </PostBody>
+    </PostWrapper>
+  );
+};
+
+export default Post;
