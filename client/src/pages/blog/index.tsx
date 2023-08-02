5263c4d018fb63dea28fde79ca26ae741cc8d710
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-08-02 22:24:51 +0330

    blog index page updated.

--- client/src/pages/blog/index.tsx
+++ client/src/pages/blog/index.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
 import { useTranslation } from "next-i18next";
-import { Grid } from "@mui/material";
 import { NextPage } from "next";
 import Head from "next/head";
 import PaginationItem from "src/components/website/Pagination";
 import Container from "src/components/website/Container";
 import Post from "src/components/website/Post";
-import { StyledTitle } from "src/components/website/common/styles";
-import MainPageLayout from "src/layouts/website/MainPageLayout";
+import InternalPageLayout from "src/layouts/website/InternalPageLayout";
 import { useAppDispatch } from "src/state/hooks/hooks";
 import { useSelector } from "react-redux";
 import useLocale from "src/hooks/useLocale";
@@ -16,24 +14,41 @@ import { getAllPublishedPostsByParams } from "src/state/slices/contentManagement
 import ApplicationParams from "src/constants/applicationParams";
 import { PostDTO } from "src/models/contentManagement/post/postDTO";
 import { useRouter } from "next/router";
+import { container } from 'src/styles/jss/globalStyle';
+import { makeStyles } from 'tss-react/mui';
+import BlockHeader from "src/components/website/BlockHeader/BlockHeader";
+import CommonMessage from "src/constants/commonMessage";
+import GridContainer from "src/components/website/Grid/GridContainer";
+import GridItem from "src/components/website/Grid/GridItem";
 
-type Props = {
-  
-};
+const styles = makeStyles()((theme) => ({
+  container,
+  mainContainer:{
+    marginLeft: 0,
+    marginRight: 0
+  },
+  itemContainer:{
+    paddingLeft: "0 !important",
+    paddingRight: "0 !important"
+  },
+  postItem: {
+    paddingBottom: "0.75rem"
+  }
+}))
 
-const Blog: NextPage<Props> = () => {
-  const router = useRouter();console.log(router)
+const Blog: NextPage = () => {
   const [paginationModel, setPaginationModel] = React.useState<any>({
     page: 0,
     pageSize: ApplicationParams.GridDefaultPageSize
   });
-
+  
   const [sortModel, setSortModel] = React.useState<any>([
     {
       field: ApplicationParams.GridDefaultSortColumn,
       sort: ApplicationParams.GridDefaultSortDirection
     },
   ]);
+  const { classes } = styles();
 
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
@@ -62,29 +77,19 @@ const Blog: NextPage<Props> = () => {
           content="width=device-width, initial-scale=1"
         ></meta>
         <meta name="description" content="Read our Blog"></meta>
-        <title>Blog</title>
-        <meta property="og:url" content="https://test.test/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content="" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="" />
-        <meta property="twitter:url" content="" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="" />
+        <title>{t("blog", CommonMessage.Blog)!}</title>
       </Head>
-      <Container>
-        <StyledTitle>{t("blog")}</StyledTitle>
-        <Grid container spacing={3}>
+      <Container className={classes.container}>
+        <GridContainer spacing={3} className={classes.mainContainer}>
+        <GridItem lg={12} className={classes.itemContainer}>
+          <BlockHeader title={t("blog", CommonMessage.Blog)!} iconCssClass="article"/>
+        </GridItem>
           {posts?.map((post: PostDTO, index: number) => (
-            <Grid item xs={12} sm={6} md={6} lg={6} key={index}>
+            <GridItem xs={12} sm={6} md={6} lg={6} key={index} className={classes.postItem}>
               <Post post={post} />
-            </Grid>
+            </GridItem>
           ))}
-        </Grid>
+        </GridContainer>
         <PaginationItem
           pagesCount={Math.ceil(totalCount/ApplicationParams.BlogDefaultPageSize)}
           currentPage="1"
@@ -95,5 +100,5 @@ const Blog: NextPage<Props> = () => {
   );
 };
 
-Blog.getLayout = (page: React.ReactNode) => <MainPageLayout>{ page }</MainPageLayout>
+Blog.getLayout = (page: React.ReactNode) => <InternalPageLayout>{ page }</InternalPageLayout>
 export default Blog;
