3897bc6684843aaf5587b59ffb18ee56ec2aa96c
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-08-02 22:24:18 +0330

    blog url page updated.

--- client/src/pages/blog/[url].tsx
+++ client/src/pages/blog/[url].tsx
@@ -20,8 +20,7 @@ import {
   Topic,
   TopicFooter,
   DateWrapper,
-  TopicWrapper,
-  StyledTitle,
+  TopicWrapper
 } from "src/components/website/common/styles";
 import { DateText } from "src/components/website/Post/styles";
 import {
@@ -35,13 +34,37 @@ import useLocale from "src/hooks/useLocale";
 import localizationService from "src/services/shared/localizationService";
 import { useSelector } from "react-redux";
 import { useRouter } from 'next/router';
-import MainPageLayout from "src/layouts/website/MainPageLayout";
+import InternalPageLayout from "src/layouts/website/InternalPageLayout";
 import SimilarPost from "src/components/website/SimilarPost";
 import { TagDTO } from "src/models/contentManagement/tag/tagDTO";
-
+import { container } from 'src/styles/jss/globalStyle';
+import { makeStyles } from 'tss-react/mui';
+import GridContainer from "src/components/website/Grid/GridContainer";
+import GridItem from "src/components/website/Grid/GridItem";
+import MostPopularPostList from "src/components/website/MostPopularPostList/MostPopularPostList";
+import BlockHeader from "src/components/website/BlockHeader/BlockHeader";
 interface Props extends PopUpT {
   
 }
+
+  const styles = makeStyles()((theme) => ({
+    container,
+    mainContainer:{
+      marginLeft: 0,
+      marginRight: 0
+    },
+    itemContainer:{
+      paddingLeft: "0 !important",
+      paddingRight: "0 !important",
+    },
+    postCntent:{
+      textAlign: "justify"
+    },
+    relatedPostTitle:{
+      marginTop: 50,
+      marginBottom: 20
+    }
+  }))
 
 const Post: NextPage<Props> = ({ setPopup, popup }) => {
   const { getLocale } = useLocale();
@@ -53,6 +76,7 @@ const Post: NextPage<Props> = ({ setPopup, popup }) => {
   const { post,  isLoading } = useSelector((state: any) => state?.blog?.post ? state?.blog : { post: null, isLoading: false });
   const router = useRouter();
   const slugUrl: string = router.query.url as string;
+  const { classes } = styles();
 
   useEffect(() => {
     if (slugUrl) {
@@ -91,60 +115,69 @@ const Post: NextPage<Props> = ({ setPopup, popup }) => {
         <meta name="twitter:image" content={post?.image ?? ''} />
       </Head>
       <Content>
-        <Container>
-          <TopicWrapper>
-            <Topic>
-              <TopicTitle>{post?.title}</TopicTitle>
-              {post?.image ? (
-                <ImageListItem key={post?.id}>
-                  <img
-                    src={`${post?.image}`}
-                    alt={post?.title}
-                    title={post?.title}
-                  />
-                </ImageListItem>
-              ) : (
-                <StyledBlockPlaceholder />
-              )}
-              <div className={style.reactMarkDown}>
-                {Parser().parse(post?.content)}
-              </div>
-              <PostLine />
-              <TopicFooter>
-                <StyledStack direction="row">
-                  {post?.postTags?.map((p: TagDTO, index: number) => (
-                    <StyledChip key={index}>{p.name}</StyledChip>
-                  ))}
-                </StyledStack>
-                <DateWrapper>
-                  <ClockIcon />
-                  <DateText>
-                  {localizationService.getLocalDateTime(post?.createdAt?.toString(), post?.locale)}
-                  </DateText>
-                </DateWrapper>
-              </TopicFooter>
-            </Topic>
+        <Container className={classes.container}>
+          <GridContainer className={classes.mainContainer}>
+            <GridItem lg={8} md={8} sm={8} xs={12} className={classes.itemContainer}>
+              <TopicWrapper>
+              <Topic>
+                <TopicTitle>{post?.title}</TopicTitle>
+                {post?.image ? (
+                  <ImageListItem key={post?.id}>
+                    <img
+                      src={`${post?.image}`}
+                      alt={post?.title}
+                      title={post?.title}
+                    />
+                  </ImageListItem>
+                ) : (
+                  <StyledBlockPlaceholder />
+                )}
+                <div className={classes.postCntent}>
+                  {Parser().parse(post?.content)}
+                </div>
+                <PostLine />
+                <TopicFooter>
+                  <StyledStack direction="row">
+                    {post?.postTags?.map((p: TagDTO, index: number) => (
+                      <StyledChip key={index}>{p.name}</StyledChip>
+                    ))}
+                  </StyledStack>
+                  <DateWrapper>
+                    <ClockIcon />
+                    <DateText>
+                    {localizationService.getLocalDateTime(post?.createdAt?.toString(), post?.locale)}
+                    </DateText>
+                  </DateWrapper>
+                </TopicFooter>
+              </Topic>
 
-            {!!post?.relatedPosts.length && (
-              <>
-                <StyledTitle>{t("relatedPosts")}</StyledTitle>
-                <Grid container spacing={3}>
-                  {post?.relatedPosts?.map((post: PostDTO, index: number) => {
-                    return (
-                      <Grid item xs={12} sm={12} md={12} lg={12} key={index}>
-                        <SimilarPost post={post} />
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              </>
-            )}
-          </TopicWrapper>
+              {!!post?.relatedPosts.length && (
+                <>
+                  <div className={classes.relatedPostTitle}>
+                    <BlockHeader title={t("relatedPosts")} iconCssClass="share"/>
+                  </div>               
+                  <Grid container spacing={3}>
+                    {post?.relatedPosts?.map((post: PostDTO, index: number) => {
+                      return (
+                        <Grid item xs={12} sm={12} md={12} lg={12} key={index}>
+                          <SimilarPost post={post} />
+                        </Grid>
+                      );
+                    })}
+                  </Grid>
+                </>
+              )}
+            </TopicWrapper>
+            </GridItem>
+            <GridItem lg={4} md={4} sm={4} xs={12} className={classes.itemContainer}>
+              <MostPopularPostList postCount={11} />
+            </GridItem>
+          </GridContainer>
         </Container>
       </Content>
       <PopUpDevelopment popup={popup} setPopup={setPopup} />
     </>
   );
 };
-Post.getLayout = (page: React.ReactNode) => <MainPageLayout>{page}</MainPageLayout>
+Post.getLayout = (page: React.ReactNode) => <InternalPageLayout>{page}</InternalPageLayout>
 export default Post;
