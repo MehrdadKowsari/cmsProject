85fb1da362aa58c7e4dd9bd83e24367341713d05
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-08-02 22:23:44 +0330

    internalPageLayout added.

---
+++ client/src/layouts/website/InternalPageLayout.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, useEffect, useState } from 'react'
+import { MainWrapper, StyledContent, Cover} from "src/components/website/common/styles";
+import Header from 'src/components/website/Header'
+import Footer from 'src/components/website/Footer/Footer'
+import Cookies from "src/components/website/Cookies";
+import initLocalization from "src/i18n";
+import { languages } from 'src/i18n/settings';
+import useLocale from 'src/hooks/useLocale';
+import ApplicationParams from 'src/constants/applicationParams';
+
+initLocalization(); 
+type InternalPageLayoutProps = {
+  children: ReactNode;
+}
+
+const InternalPageLayout = ({ children }: InternalPageLayoutProps) => {
+  const [popup, setPopup] = useState<boolean>(false);
+  const { getLocale } = useLocale();
+  const locale = getLocale();
+  
+  const dir: string = languages.find(p => p.code === locale)?.dir || ApplicationParams.DefaultLanguageDirection;
+  useEffect(() => {
+    document.body.dir = dir;
+  }, [])
+  
+  return (
+    <>
+      {popup && <Cover></Cover>}
+            <MainWrapper>
+              <Header />
+              <StyledContent>
+                  {children}
+              </StyledContent>
+              <Cookies />
+              <Footer />
+            </MainWrapper>
+    </>
+  )
+}
+export default InternalPageLayout;
