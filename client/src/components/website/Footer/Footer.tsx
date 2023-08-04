d8e29f5cd7898fbb12bd9080249e7c466f9ad299
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-08-04 23:36:51 +0330

    footer extera imports updated.

--- client/src/components/website/Footer/Footer.tsx
+++ client/src/components/website/Footer/Footer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import {
-  ImageLink,
   InfoText,
   StyledFooterContainer,
   MainFooterContent,
-  Socials,
   StyledFooter,
   StyledCopyRightContainer
 } from "./styles";
