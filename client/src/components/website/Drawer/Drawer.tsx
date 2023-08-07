8974da4cc9e66cda9a79b0e160907a91b13b5478
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-08-07 22:02:08 +0330

    drawer vertical show issue resolved.

--- client/src/components/website/Drawer/Drawer.tsx
+++ client/src/components/website/Drawer/Drawer.tsx
@@ -5,14 +5,17 @@ import { makeStyles } from "tss-react/mui";
 import MenuIcon from "@mui/icons-material/Menu";
 import { MenuItemDTO } from "src/models/contentManagement/menuItem/menuItemDTO";
 import Icon from '@mui/material/Icon';
-const useStyles = makeStyles()(() => ({
+const useStyles = makeStyles()((theme) => ({
+    root:{
+      minWidth: 250
+    },
     link:{
         textDecoration:"none",
-        color: "blue",
+        color: "#222",
         fontSize: "20px",
     },
     icon:{
-        color: "white"
+        color: theme.palette.primary.main
     }
 }));
 type DrawerProps = {
@@ -28,19 +31,23 @@ const DrawerComponent = ({ items: menuItems}: DrawerProps) => {
         open={openDrawer}
         onClose={() => setOpenDrawer(false)}
       >
-        <List>
-        <ListItem onClick={() => setOpenDrawer(false)}>
-            <ListItemText>
-            {
-                  menuItems?.map((p: MenuItemDTO) => (
-                    <>
-                      <Link href={p.url} className={classes.link} key={p.id}> <Icon className={classes.icon}>{p.iconCssClass}</Icon> {p.name}</Link>
-                    </>
-                  ))
-                }
-            </ListItemText>
-          </ListItem>
-          <Divider/>
+        <List className={classes.root}>
+          <ListItem onClick={() => setOpenDrawer(false)}>
+              <ListItemText>
+              {
+                <List>{
+                    menuItems?.map((p: MenuItemDTO) => (
+                      <>
+                        <ListItem key={p.id}>
+                          <Link href={p.url} className={classes.link} key={p.id}> <Icon className={classes.icon}>{p.iconCssClass}</Icon> {p.name}</Link>
+                        </ListItem>
+                      </>
+                    ))
+                  }
+                </List>
+              }
+              </ListItemText>
+            </ListItem>       
         </List>
       </Drawer>
       <IconButton onClick={() => setOpenDrawer(!openDrawer)}className={classes.icon}>
