92b5c1de5a261556729e01fcb5f996043e339e68
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-08-11 22:22:03 +0330

    fotter components updated.

--- client/src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ client/src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -1,51 +1,19 @@
-// ** MUI Imports
 import Box from '@mui/material/Box'
-import Link from '@mui/material/Link'
-import { Theme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
-import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTranslation } from 'next-i18next'
+import useLocale from 'src/hooks/useLocale'
+import localizationService from 'src/services/shared/localizationService'
 
 const FooterContent = () => {
-  // ** Var
-  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
+  const { t } = useTranslation();
+  const { getLocale } = useLocale();
+  const currentYear = localizationService.getCurrentYear(getLocale());
 
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
       <Typography sx={{ mr: 2 }}>
-        {`© ${new Date().getFullYear()}, Made with `}
-        <Box component='span' sx={{ color: 'error.main' }}>
-          ❤️
-        </Box>
-        {` by `}
-        <Link target='_blank' href='https://themeselection.com/'>
-          ThemeSelection
-        </Link>
+      {t("allRightsReserved")}{" "} {t("copyright")} © {currentYear}
       </Typography>
-      {hidden ? null : (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
-          >
-            MIT License
-          </Link>
-          <Link target='_blank' href='https://themeselection.com/'>
-            More Themes
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
-          >
-            Documentation
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
-          >
-            Support
-          </Link>
-        </Box>
-      )}
     </Box>
   )
 }
--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import { Box, Typography } from '@mui/material'
+import { useTranslation } from 'next-i18next';
 import React from 'react'
+import useLocale from 'src/hooks/useLocale';
+import localizationService from 'src/services/shared/localizationService';
 
 const Footer = () => {
+  const { t } = useTranslation();
+  const { getLocale } = useLocale();
+  const currentYear = localizationService.getCurrentYear(getLocale());
   return (
     <Box>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              All Right Reserved &copy; 2022
+        {t("allRightsReserved")}{" "} {t("copyright")} © {currentYear}
         </Typography>
     </Box>
   )
