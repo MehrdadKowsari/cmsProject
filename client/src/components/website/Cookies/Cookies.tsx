4d00388fe7fccb3d8ef26973bb9fff639a035979
mehrdad.kowsari <mehrdad.kowsari@gamil.com>
2023-09-26 20:48:59 +0330

    cookie component updated.

--- client/src/components/website/Cookies/Cookies.tsx
+++ client/src/components/website/Cookies/Cookies.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "next-i18next";
-import { Text, CookiesBox, Button, CoockieImg } from "./styles";
+import CookieIcon from '@mui/icons-material/Cookie';
+import { Text, CookiesBox, Button } from "./styles";
+import { makeStyles } from "tss-react/mui";
+
+const styles = makeStyles()((theme) => ({
+  primaryIcon: {
+    color: theme.palette.primary.main,
+    fontSize: "2rem"
+  },
+}));
 
 const Cookies: React.FC = () => {
   const { t } = useTranslation();
   const [allowedCookies, setAllowedCookies] = useState(true);
+  const { classes }= styles();
   useEffect(() => {
     return setAllowedCookies(
       localStorage?.getItem("allowedCookies") === "true"
@@ -16,7 +26,7 @@ const Cookies: React.FC = () => {
         ""
       ) : (
         <CookiesBox>
-          <CoockieImg src="/images/coockie.svg" alt="cookie" />
+          <CookieIcon className={classes.primaryIcon} />
           <Text>{t("cookieText")}</Text>
           <Button
             onClick={() => {
